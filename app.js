e7acb9f90df8b6fb861bf340daafa57ef15f51b3
Connor Abdelnoor & Tom Mertz & Tanner Heffner <student@epicodus.com>
2015-04-30 11:23:19 -0700

    Add angular router.

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
-var starSounds = angular.module('starSounds', []);
+var starSounds = angular.module('starSounds', ['ngRoute']);
 
-    starSounds.config(function($routeProvider))
+    starSounds.config(function($routeProvider, $locationProvider) {
+
+        $routeProvider
+            .when('/project1', {
+                templateUrl : "partials/project1.html"
+            })
+            .when('/project2', {
+                templateUrl : "partials/project2.html"
+            })
+            .when('/project3', {
+                templateUrl : "partials/project3.html"
+            });
+
+        $locationProvider.html5Mode(true);
+    });
